74eb1edbebbed8652656df551d46cd55be67e69e
shashank-mugiwara <shashank.j@creditsaison-in.com>
2023-11-19 06:36:08 +0530

    intel_poller: Refactoring.

--- pkg/type_connectors/connectors.go
+++
@@ -1,6 +0,0 @@
-package type_connectors
-
-type ConnectorConfigRequest struct {
-	Name   string            `json:"name" validate:"required" `
-	Config map[string]string `json:"config" validate:"required"`
-}
